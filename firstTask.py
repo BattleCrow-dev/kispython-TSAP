79ee00abc3516b258548d8cb04d2ecdc9ce22856
BattleCrow <pashkovvv00@gmail.com>
2024-02-09 18:33:19 +0300

    Solution of first task

---
+++ firstTask.py
@@ -0,0 +1,47 @@
+# from math import sin, atan, tan
+
+
+# 1-ое по популярности
+# def main(y, x, z):
+#     first = 54 * (35 * z ** 2 - 5 * x ** 3) ** 2
+#     second = 29 * math.sin(92 * y ** 3 + 1) ** 5
+#     third = 15 * math.atan(y ** 2 - z ** 3 / 71) ** 5
+#     fourth = 78 * math.tan(26 * x ** 3 - 71 * x ** 2) ** 4
+#     fifth = (x + z ** 2 / 13 + 62) ** 2 / 88 - 13 * y ** 5
+#     return first + second - (third + fourth) / fifth
+
+
+# 2-ой по популярности
+# def main(y, x, z):
+#     first = 54 * (35 * z**2 - 5 * x**3)**2
+#     second = 29 * sin(92 * y**3 + 1) ** 5
+#     third = 15 * atan(y**2 - z**3 / 71) ** 5
+#     fourth = 78 * tan(26 * x**3 - 71 * x**2)**4
+#     fifth = (x + z**2 / 13 + 62)**2 / 88 - 13 * y**5
+#
+#     return first + second - (third + fourth) / fifth
+
+
+# 3-ое по популярности
+# def main(y, x, z):
+#    return 54 * (35 * z ** 2 - 5 * x ** 3) ** 2 + 29 * math.sin(92 * y ** 3 + 1) ** 5 - \
+#        (15 * math.atan(y ** 2 - z ** 3 / 71) ** 5 + 78 * math.tan(26 * x ** 3 - 71 * x ** 2) ** 4) / \
+#        ((x + z ** 2 / 13 + 62) ** 2 / 88 - 13 * y ** 5)
+
+
+# 4-ое по популярности
+# def main(y, x, z):
+#     return 54 * (35 * z ** 2 - 5 * x ** 3) ** 2 + 29 * sin(92 * y ** 3 + 1) ** 5 - \
+#         (15 * atan(y ** 2 - z ** 3 / 71) ** 5 + 78 * tan(26 * x ** 3 - 71 * x ** 2) ** 4) / \
+#         ((x + z ** 2 / 13 + 62) ** 2 / 88 - 13 * y ** 5)
+
+
+# def test():
+#     print(main(0.1, 0.44, -0.37))
+#     print(main(-0.81, -0.51, -0.4))
+#     print(main(0.1, 0.6, -0.0))
+#     print(main(0.99, 0.81, 0.88))
+#     print(main(0.68, 0.48, -0.42))
+
+
+# test()
